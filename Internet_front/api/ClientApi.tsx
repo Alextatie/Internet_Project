86c8b2554014bd9e2e1b52c5a353d5e60180fcbe
Alex Tatievsky <alextatie@gmail.com>
2024-05-16 12:19:24 +0300

    not sure

--- Internet_front/api/ClientApi.tsx
+++ Internet_front/api/ClientApi.tsx
@@ -1,6 +1,5 @@
 import { create } from "apisauce";
 const apiClient = create({
-    baseURL: 'http://10.0.0.9:3000',
     headers: { Accept: 'application/vnd.github.v3+json' },
 })
 export default apiClient
